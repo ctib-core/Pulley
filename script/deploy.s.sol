98220212c1de3b22eaafe39cfe450e005fd67ec9
kaleel <pixelhubster@gmail.com>
2025-08-12 23:45:01 +0000

    feat: deploy script coredao

---
+++ script/deploy.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+
+import {Script} from "forge-std/src/Script.sol";
+import {PulleyTokenEngine} from "../src/Token/pulleyEngine.sol";
+import {PulleyToken} from "../src/Token/pulleyToken.sol";
+import {CrossChainController} from "../src/cross_chain/cross_chain_controller.sol";
+import {Gateway} from "../src/Gateway.sol";
+contract DeployScript is Script {
+  // deploy on coredao
+
+  PulleyTokenEngine pulley;
+  PulleyToken pToken;
+  PermissionManager permissionManager;
+  CrossChainController controller;
+  address[] allowedAssetsList;
+  address baseEndpoint;
+  address owner;
+  TradingPool tradingPool;
+  address[] supportedAssets;
+  Gateway gateway;
+
+
+  function setUp() {
+    controller = new CrossChainController(baseEndpoint, owner);
+    pToken = new PulleyToken("PulleyToken", "PK");
+    pulley = new PulleyTokenEngine(address(ptoken), allowedAssetsList, address(permissionManager));
+    tradingPool = new TradingPool(address(pulley), supportedAssets, address(permissionManager));
+    gateway = new Gateway(address(pToken), address(pulley), address(tradingPool), address(controller), address(permissionManager));
+
+  }
+
+
+}
